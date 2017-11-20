c3819e5a22220c346c72298ec6a902e442d97e86
Kane Marshall <kanemars@hotmail.com>
2017-11-20 10:53:53 +0000

    Shorten the amount of code required to send a Notification

--- Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
+++ Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
@@ -31,6 +31,60 @@ public class MainActivity extends Activity {
     }
 
     /**
+     * The smallest amount of code that will send a notification to Android Auto
+     * @param view
+     */
+    public void sendNotificationToAndroidAuto (View view) {
+
+        int thisConversationId = 42;
+        Intent msgHeardIntent = new Intent().addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+                .setAction("com.example.android.basicnotifications.KANE_ACTION_MESSAGE_HEARD")
+                .putExtra("conversation_id", thisConversationId);
+
+        PendingIntent msgHeardPendingIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                thisConversationId,
+                msgHeardIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Intent msgReplyIntent = new Intent().addFlags(Intent.FLAG_INCLUDE_STOPPED_PACKAGES)
+                .setAction("com.example.android.basicnotifications.KANE_ACTION_MESSAGE_REPLY")
+                .putExtra("conversation_id", thisConversationId);
+
+        PendingIntent msgReplyPendingIntent = PendingIntent.getBroadcast(getApplicationContext(),
+                thisConversationId,
+                msgReplyIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        RemoteInput remoteInput = new RemoteInput.Builder("voice_reply_key")
+                .setLabel("Prompt text")
+                .build();
+
+        String conversation = "Chuff Me";
+        NotificationCompat.CarExtender.UnreadConversation.Builder unreadConvBuilder =
+                new NotificationCompat.CarExtender.UnreadConversation.Builder(conversation)
+                        .setReadPendingIntent(msgHeardPendingIntent)
+                        .setReplyAction(msgReplyPendingIntent, remoteInput);
+
+        unreadConvBuilder.addMessage("Chuff chuff, the trains are on time!").
+                setLatestTimestamp(System.currentTimeMillis());
+
+        // Below is everything to do with the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "DummyChannelId");
+        builder.extend(new NotificationCompat.CarExtender().setUnreadConversation(unreadConvBuilder.build()));
+        builder.setSmallIcon(R.drawable.ic_stat_notification);
+        // Set the intent that will fire when the user taps the notification.
+        //builder.setContentIntent(pendingIntent);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher));
+
+        builder.setContentTitle("Notification to appear in Auto");
+        builder.setContentText("Trains are on time!");
+        builder.setSubText("More subtext");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    /**
      * Send a sample notification using the NotificationCompat API.
      */
     public void sendNotification(View view) {
@@ -40,9 +94,9 @@ public class MainActivity extends Activity {
          * The intent needs to be packaged into a {@link android.app.PendingIntent} so that the
          * notification service can fire it on our behalf.
          */
-        Intent intent = new Intent(Intent.ACTION_VIEW,
+        Intent intentWhenNotificationClickedOn = new Intent(Intent.ACTION_VIEW,
                 Uri.parse("http://developer.android.com/reference/android/app/Notification.html"));
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        PendingIntent pendingIntentWhenNotificationClickedOn = PendingIntent.getActivity(this, 0, intentWhenNotificationClickedOn, 0);
         // END_INCLUDE(build_action)
 
         int thisConversationId = 42;
@@ -83,7 +137,7 @@ public class MainActivity extends Activity {
         /**
          * Use NotificationCompat.Builder to set up our notification.
          */
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "DummyChannelId");
 
         builder.extend(new NotificationCompat.CarExtender().setUnreadConversation(unreadConvBuilder.build()));
 
@@ -100,7 +154,7 @@ public class MainActivity extends Activity {
         builder.setSmallIcon(R.drawable.ic_stat_notification);
 
         // Set the intent that will fire when the user taps the notification.
-        builder.setContentIntent(pendingIntent);
+        builder.setContentIntent(pendingIntentWhenNotificationClickedOn);
 
         // Set the notification to auto-cancel. This means that the notification will disappear
         // after the user taps it, rather than remaining until it's explicitly dismissed.
